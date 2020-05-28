b7eb20cea17d6ccf7083102d411414105cadbcbc
ricardomn23 <ricardo.nunes.233@gmail.com>
2020-05-28 11:16:25 +0100

    simulador

---
+++ test/BUVSimTest.cpp
@@ -0,0 +1,40 @@
+
+#include "BUVSim/BUVSim.hpp"
+
+#include <iostream>
+
+
+using namespace std;
+// using namespace Eigen;
+
+
+int main()
+{
+	cout << "BUVSim Demo" << endl;
+
+	float T = 1.0/18.0;   // Sampling time
+	float N = ceil(40.0/T);   // Simulation steps
+
+	BUV1_Sim buv(T, true);
+	Eigen::Matrix < float, 3, 1 > curr(3.0,4.0,-2.0);
+	buv.setSeaCurr(curr);
+	Eigen::Matrix<float,3,3> motorCom;
+	// Each mCommand column: frequency (RPM), mean value (degrees), amplitude (degrees)
+	// First column: tail fin
+	// Second column: left fin
+	// Third column: right fin
+	motorCom.col(0) << 1000.0, 30.0, 20.0;
+	motorCom.col(1) << 2500.0, 40.0, 20.0;
+	motorCom.col(2) << 2500.0, 40.0, 20.0;
+	buv.setMotorCommands(motorCom);
+
+	cout << "SeaCurr:" << endl;
+	cout << curr << endl;
+	cout << "Actuation:" << endl;
+	cout << motorCom << endl;
+
+	for(int i=0; i<N; i++)
+		buv.update();
+
+	return 0;
+}
